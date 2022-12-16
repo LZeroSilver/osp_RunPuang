98a6fe3fadde82e304733ad626f4f7aa046969c5
ahyuncho <117696129+ahyuncho@users.noreply.github.com>
2022-12-16 18:02:45 +0900

    third commit

--- button.py
+++ button.py
@@ -2,10 +2,9 @@ import pygame
 
 #button class
 class Button():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+	def __init__(self,Xsize, Ysize, x, y, image):
+		image=pygame.transform.scale(image, (Xsize, Ysize))
+		self.image = image
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 		self.clicked = False
